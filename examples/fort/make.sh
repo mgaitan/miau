5189c377a09af111b08da4baccc16c473179f409
Martin Gaitan <mgaitan@onapsis.com>
2017-05-09 11:59:40 -0300

    another example in spanish

---
+++ examples/fort/make.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/sh
+youtube-dl -f 43 https://www.youtube.com/watch?v=bra9kqtU7vk  --external-downloader ffmpeg --external-downloader-args '-t 167'
+miau Puroshow.com\ -\ El\ discurso\ de\ Fort-bra9kqtU7vk.* -r remix.txt
+# Result at https://www.youtube.com/watch?v=E_8BdQpXRLo
